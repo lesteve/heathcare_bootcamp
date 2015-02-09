58fed6a493da727045c664db8e34272298c27257
Loïc Estève <loic.esteve@ymail.com>
2015-02-09 09:45:05 +0100

    Change n_estimators to 35

--- model.py
+++ model.py
@@ -2,9 +2,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 
+
 def model(X_train, y_train, X_test):
     clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-        ('rf', RandomForestClassifier(n_estimators=30))])
+                    ('rf', RandomForestClassifier(n_estimators=35))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
